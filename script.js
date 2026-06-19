45795d97f257c5738dbd725a1637ce2521896a48
Kumaraswamy Manga <mangakumar441@gmail.com>
2026-06-19 21:26:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,26 +4,33 @@ let htno=document.getElementById("htno").value;
 
 let password=document.getElementById("password").value;
 
-let url="https://script.google.com/macros/s/AKfycbxoISPwQeHi1fTJuAnSf8X1XbeOazrqzYGkGIoI0aBEF3hzVLgD0lS4szV6N8mCjL9P/exec?action=login&htno="+htno+"&password="+password;
+let url="https://script.google.com/macros/s/AKfycbxoISPwQeHi1fTJuAnSf8X1XbeOazrqzYGkGIoI0aBEF3hzVLgD0lS4szV6N8mCjL9P/exec?action=login&htno="
++encodeURIComponent(htno)
++"&password="
++encodeURIComponent(password);
 
 fetch(url)
+
 .then(response=>response.json())
+
 .then(data=>{
 
 if(data.status=="success"){
 
 document.getElementById("msg").innerHTML=
-"Welcome "+data.name+" ("+data.branch+")";
+"Welcome "+data.name+
+" ("+data.branch+")";
 
 }
 else{
 
 document.getElementById("msg").innerHTML=
-"Invalid Hall Ticket or Password";
+"Invalid Login";
 
 }
 
 })
+
 .catch(error=>{
 
 document.getElementById("msg").innerHTML=
