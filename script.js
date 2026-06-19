aaed08f2168738831c3244d2928a42bc9ed7f202
Kumaraswamy Manga <mangakumar441@gmail.com>
2026-06-19 21:22:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+function login(){
 
+let htno=document.getElementById("htno").value;
+
+let password=document.getElementById("password").value;
+
+let url="https://script.google.com/macros/s/AKfycbxoISPwQeHi1fTJuAnSf8X1XbeOazrqzYGkGIoI0aBEF3hzVLgD0lS4szV6N8mCjL9P/exec?action=login&htno="+htno+"&password="+password;
+
+fetch(url)
+.then(response=>response.json())
+.then(data=>{
+
+if(data.status=="success"){
+
+document.getElementById("msg").innerHTML=
+"Welcome "+data.name+" ("+data.branch+")";
+
+}
+else{
+
+document.getElementById("msg").innerHTML=
+"Invalid Hall Ticket or Password";
+
+}
+
+})
+.catch(error=>{
+
+document.getElementById("msg").innerHTML=
+"Connection Error";
+
+});
+
+}
